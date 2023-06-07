0fc76b23feaa5c47615d4ee78efa95e745ee9c21
Yash Gupta <yashgupta13.in@gmail.com>
2023-06-07 12:39:39 +0530

    setting up mongodb locally

--- backend/server.js
+++ backend/server.js
@@ -6,39 +6,26 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const url = "mongodb://localhost:27017/invent";
+
+mongoose
+    .connect(url,{})
+    .then(() => {
+        app.listen(PORT,()=>{
+                console.log(`Server Running on port ${PORT}`)
+            })
+        })
+    .catch((err) => console.log(err))
 
 
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://solarladder:solarladder123@solar-ladder.baaoti5.mongodb.net/?retryWrites=true&w=majority";
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
 
 
-// Connect to DB and start server
+
+// // Connect to DB and start server
 // mongoose
-//     .connect(process.env.MONGO_URI)
+//     // .connect(process.env.MONGO_URI)
+//     .connect("mongodb://localhost:27018")
 //     .then(() => {
 //         app.listen(PORT,()=>{
 //             console.log(`Server Running on port ${PORT}`)
@@ -46,16 +33,4 @@ const PORT = process.env.PORT || 5000;
 //     })
 //     .catch((err) => console.log(err))
 
-    mongoose.connect(
-        "mongodb://localhost:27018",
-        {
-          dbName: "yourDB-name",
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        },
-        (err) =>
-          err ? console.log(err) : console.log(
-            "Connected to yourDB-name database")
-      );
-
-    // console.log(process.env.MONGO_URI)
+  
